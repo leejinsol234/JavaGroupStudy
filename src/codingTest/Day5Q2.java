00321c323ac6d1a14e4636d113ce940fb9c02dab
leejinsol234 <desirable7@naver.com>
2024-02-22 15:15:35 +0900

    프로그래머스 day5

---
+++ src/codingTest/Day5Q2.java
@@ -0,0 +1,17 @@
+package codingTest;
+
+public class Day5Q2 {
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+
+        int num = a;
+        for(int i = 0; i<included.length; i++){
+            if(included[i]){
+                answer += num;
+            }
+
+            num += d;
+        }
+        return answer;
+    }
+}
---
+++ src/codingTest/Day5Q4.java
@@ -0,0 +1,23 @@
+package codingTest;
+
+public class Day5Q4 {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int multi = 1;
+
+        for(int i=0; i<num_list.length; i++){
+            sum += num_list[i];
+            multi *= num_list[i];
+        };
+
+        sum = sum*sum;
+
+        if(multi < sum){
+            answer = 1;
+        } else{
+            answer = 0;
+        }
+        return answer;
+    }
+}

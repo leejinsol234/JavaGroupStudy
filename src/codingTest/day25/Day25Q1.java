df68d5fc9f7c706ba9d1d47bda667f266fb211a9
leejinsol234 <desirable7@naver.com>
2024-04-23 16:55:48 +0900

    코딩테스트 day25

---
+++ src/codingTest/day25/Day25Q1.java
@@ -0,0 +1,36 @@
+package codingTest.day25;
+
+public class Day25Q1 {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+
+        int num = 1;
+        int rowS = 0;
+        int rowE = n-1;
+        int colS = 0;
+        int colE = n-1;
+
+        while(num<=n*n){
+            for(int i=colS; i<=colE; i++){
+                answer[rowS][i] = num++;
+            }
+            rowS++;
+
+            for(int i=rowS; i<=rowE; i++){
+                answer[i][colE] = num++;
+            }
+            colE--;
+
+            for(int i=colE; i>=colS; i--){
+                answer[rowE][i] = num++;
+            }
+            rowE--;
+
+            for(int i=rowE; i>=rowS; i--){
+                answer[i][colS] = num++;
+            }
+            colS++;
+        }
+        return answer;
+    }
+}
